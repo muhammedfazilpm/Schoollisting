e20b716872a39d4fda998c41fe61c7e832c955d5
Muhammed Fazil pm <fazilfezz@gmail.com>
2023-11-10 07:42:06 +0530

    lastss

--- back-end/config/dbConfig.js
+++ back-end/config/dbConfig.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 const connection=mongoose.connection
 
-connection.on("connected",()=>console.log("database connected"))
+connection.on("connected",()=>console.log("database connected as "))
 connection.on("error",(error)=>console.log(error))
 
 module.exports=mongoose
